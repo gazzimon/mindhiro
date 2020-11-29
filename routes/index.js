9778e67d000ee805f520d56cba7d1ed0b94228d0
Gustavo Azzimonti Renzo <gazzimon@gmail.com>
2020-11-29 20:05:54 -0300

    register html

--- routes/index.js
+++ routes/index.js
@@ -10,8 +10,13 @@ router.get("/mindhiro", function(req, res, next) {
   res.render("mindhiro", { title:  "mindhiro" });
 });
 
+router.get("/landing", function(req, res, next) {
+  res.render("landing", { title:  "landing" });
+});
+
 router.get("/preguntas", function(req, res, next) {
   res.render("preguntas", { title:  "preguntas" });
 });
+
 
 module.exports = router;
